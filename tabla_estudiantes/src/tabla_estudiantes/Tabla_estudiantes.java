f0a53a3ffc2df09e71a850eaccfd5945e53afb87
u1803391 <60480527+u1803391@users.noreply.github.com>
2020-05-01 13:30:22 -0500

    Add files via upload

---
+++ tabla_estudiantes/src/tabla_estudiantes/Tabla_estudiantes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabla_estudiantes;
+
+/**
+ *
+ * @author crist
+ */
+public class Tabla_estudiantes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        vista vista=new vista();
+        vista.setVisible(true);
+    }
+    
+}
